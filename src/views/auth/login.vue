97f3a97c2c923bf83ab094c195e899d212b3748c
AQodS <qoddri@gmail.com>
2025-05-24 22:51:10 +0700

    feat: view & route auth

---
+++ src/views/auth/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="p-5 mb-4 bg-light rounded-3 shadow-sm">
+    <div class="conainer-fluid py-5">
+      <h1 class="display-5 fw-bold">Login Page</h1>
+      <p class="col-md-12 fs-4">Fullstack with golang & Vue</p>
+    </div>
+  </div>
+</template>
+
+<style lang=""></style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-5 mb-4 bg-light rounded-3 shadow-sm">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">Fullstack Web Developer</h1>
+      <p class="col-md-12 fs-4">Fullstack with golang & Vue</p>
+      <hr />
+      <router-link to="/register" class="btn btn-primary btn-lg me-3">
+        Register
+      </router-link>
+      <router-link to="/login" class="btn btn-secondary btn-lg">
+        Login
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style lang=""></style>
